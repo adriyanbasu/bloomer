79cd05c1c8b130d9678576e83c63436ff462cefe
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-03-03 19:19:15 +0000

    a commit a

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"unicode"
+)
 
-func main() {
-	fmt.Println("Hello, Go in CodeSandbox!")
+type tokenKind uint
+
+const (
+	syntaxtoken tokenKind = iota
+	identiferToken
+	integerToken
+)
+
+type token struct {
+	value string
+	kind  tokenkind
+}
+
+// *(+ 13 2)  *(15 and 2 = 17) **!(identiferToken)
+//
+
+func eatwhitespace(source []rune, curser int) int {
+	for curser < len(source) {
+		if unicode.IsSpace(source[curser]) {
+			curser++
+			continue
+		}
+		break
+	}
+}
+
+func iswhitespace(r rune) bool {
+
+}
+
+func lex(source []rune) []token {
+	var tokens []token
+
+	curser := 0
+	for {
+		curser = eatwhitespace(source, curser)
+	}
 }
